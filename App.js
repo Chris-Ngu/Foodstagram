5e7294f0f56aab5587cc77d624fdb06c7258aa43
Christopher Nguyen <Chris_ngu@pm.me>
2020-04-13 17:37:04 -0500

    Setting up firebase

---
+++ App.js
@@ -0,0 +1,108 @@
+import * as firebase from 'firebase';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, SnapshotViewIOS } from 'react-native';
+import Constants from 'expo-constants';
+
+import fbaseConfig from './src/fbaseConfig';
+fbaseConfig.init();
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: '',
+      messages: []
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  componentDidMount() {
+    //Initially gets messages
+    firebase
+      .database()
+      .ref()
+      .child("messages")
+      .once('value', snapshot => {
+        const data = snapshot.val()
+        if (snapshot.val()) {
+          const initMessages = [];
+          Object.keys(data).forEach(message => initMessages.push(data[message]));
+          this.setState({ messages: initMessages });
+        }
+      });
+
+    //Listener to check if more messages were added
+    firebase
+      .database()
+      .ref()
+      .child("messages")
+      .on("child_added", snapshot => {
+        const data = snapshot.val();
+        if (data) {
+          this.setState(prevState => ({
+            messages: [data, ...prevState.messages]
+          }))
+        }
+      })
+  }
+
+  addItem() {
+    if (!this.state.message) return;
+
+    const newMessage = firebase.database().ref()
+      .child("messages")
+      .push();
+    newMessage.set(this.state.message, () => this.setState({ message: '' }))
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.msgBox}>
+          <TextInput placeholder='Enter message here' style={styles.txtInput} onChangeText={(text) => this.setState({ message: text })} />
+          <Button title='Send' onPress={this.addItem} />
+        </View>
+        <FlatList data={this.state.messages}
+          renderItem={
+            ({ item }) =>
+              <View style={styles.listItemContainer}>
+                <Text style={styles.listItem}>
+                  {item}
+                </Text>
+              </View>
+          }
+
+        />
+      </View>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+    marginTop: Constants.statusBarHeight
+  },
+  msgBox: {
+    flexDirection: 'row',
+    padding: 20,
+    backgroundColor: '#fff'
+  },
+  txtInput: {
+    flex: 1
+  },
+  listItemContainer: {
+    backgroundColor: '#fff',
+    margin: 5,
+    borderRadius: 5
+  },
+  listItem: {
+    fontSize: 20,
+    padding: 10
+  }
+});
---
+++ src/fbaseConfig.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyAFIyVKNti6UfMzBWMLOE_g-HfGIySZ25I",
+    authDomain: "appdemo-3244a.firebaseapp.com",
+    databaseURL: "https://appdemo-3244a.firebaseio.com",
+    projectId: "appdemo-3244a",
+    storageBucket: "appdemo-3244a.appspot.com",
+    messagingSenderId: "508380174685",
+    appId: "1:508380174685:web:1b9329e3f0cbe0a47cf0b4",
+    measurementId: "G-KMS4165V3H"
+};
+
+export default class fbaseConfig {    
+    static init() {
+        firebase.initializeApp(config);
+    }
+}
