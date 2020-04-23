91b48f07bb689acd6be8694818c7f65399c28b88
Christopher Nguyen <chris_ngu@pm.me>
2020-04-22 20:10:47 -0500

    Editing

--- src/fbaseConfig.js
+++ src/fbaseConfig.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 const config = {
-    apiKey: "AIzaSyAFIyVKNti6UfMzBWMLOE_g-HfGIySZ25I",
-    authDomain: "appdemo-3244a.firebaseapp.com",
-    databaseURL: "https://appdemo-3244a.firebaseio.com",
-    projectId: "appdemo-3244a",
-    storageBucket: "appdemo-3244a.appspot.com",
-    messagingSenderId: "508380174685",
-    appId: "1:508380174685:web:1b9329e3f0cbe0a47cf0b4",
-    measurementId: "G-KMS4165V3H"
+    apiKey: " ",
+    authDomain: " ",
+    databaseURL: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " ",
+    measurementId: " "
 };
 
 export default class fbaseConfig {
